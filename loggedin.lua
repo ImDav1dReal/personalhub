d92f3416a8a130a2d1d540809ec4676d37d94e38
ImDav1dReal <115368785+ImDav1dReal@users.noreply.github.com>
2022-11-06 06:50:43 -0800

    Update loggedin.lua

--- loggedin.lua
+++ loggedin.lua
@@ -2,48 +2,23 @@ local plr = game.Players.LocalPlayer
 local OSTime = os.time()
 local Time = os.date('!*t', OSTime)
 
-local function getexploit()
-    local exploit =
-        (syn and not is_sirhurt_closure and not pebc_execute and "Synapse X") or 
-        (isexecutorclosure and "Script-Ware V2") or
-        (secure_load and "Sentinel") or
-        (is_sirhurt_closure and "SirHurt V4") or
-        (pebc_execute and "ProtoSmasher") or
-        (KRNL_LOADED and "Krnl") or
-        (WrapGlobal and "WeAreDevs") or
-        (isvm and "Proxo") or
-        (shadow_env and "Shadow") or
-        (jit and "EasyExploits") or
-        (getreg()['CalamariLuaEnv'] and "Calamari") or
-        (unit and "Unit") or
-        ("Undetectable")
-    return exploit
-end
-
---Please don't spam the webhook. I don't even log IP's :/
 local Embed = {
-			["title"] = "__**Passed KeySystem !**__",
-			["description"] = "Name: "..plr.Name.."\nDisplay Name: "..plr.DisplayName..",
+			["title"] = "__**Passed KeySystem!**__",
+			["description"] = "Name: "..plr.Name.."\nDisplay Name: "..plr.DisplayName.."",
 			["type"] = "rich",
-			["color"] = tonumber(0x00FF00),
+			["color"] = tonumber(0x00ff00),
 	                ["thumbnail"] = {
-				["url"] = "https://www.roblox.com/asset-thumbnail/image?assetId="..game.PlaceId.."&width=768&height=432"
+				["url"] = ""
 			},
 			["image"] = {
 				["url"] = "http://www.roblox.com/Thumbs/Avatar.ashx?x=250&y=250&Format=Png&username="..plr.Name
 			},
 			["fields"] = {
-				{
-					["name"] = "__User ID:__",
-					["value"] = plr.UserId,
-					["inline"] = true
-				}
+				
 			},
 			["footer"] = {
-			    ["text"] = "Personal Hub",
-			    ["icon_url"] = ""
 			},
-			["timestamp"] = string.format('%d-%d-%dT%02d:%02d:%02dZ', Time.year, Time.month, Time.day, Time.hour, Time.min, Time.sec),
+			
 };
 (syn and syn.request or http_request or http.request) {
     Url = 'https://discord.com/api/webhooks/1038798301249683509/E7eSR4T1tn4O5kltNfhT2B6YV1pbKubql7wChPAPkUCyo0SoDXUl8EhuXbWnHgHuKGUB';
